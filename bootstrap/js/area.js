b3284169a73093c6bea9caaaf5892d4a9bb18891
RobertFlame <zzq2362@live.com>
2022-06-01 03:49:15 +0200

    fix shift problem

--- bootstrap/js/area.js
+++ bootstrap/js/area.js
@@ -39,8 +39,8 @@ function plot_area(countries, height, width) {
             depth: 300,
             viewDistance:30
           },
-          "height": Math.max(height, 50),
-          "width": Math.max(width, 300),
+          "height": Math.min(Math.max(height, 50), 300),
+          "width": Math.min(Math.max(width, 300), 800),
           backgroundColor: 'transparent',
         },
         title: {
